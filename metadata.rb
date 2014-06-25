06a5fcfa7604a89c11f7f93c65071e076cfeeeff
Brady Wied <brady@wied.us>
2014-06-25 10:22:14 -0500

    Ran meez -C "BSW Technology Consulting LLC" -m support@bswtechconsulting.com nginx

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'nginx'
+maintainer       'BSW Technology Consulting LLC'
+maintainer_email 'support@bswtechconsulting.com'
+license          ''
+description      'Installs/Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,7 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2014, BSW Technology Consulting LLC
+#
